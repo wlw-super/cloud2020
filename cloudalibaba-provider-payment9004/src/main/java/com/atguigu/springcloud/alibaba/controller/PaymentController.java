4e4feb16ab18c2de03df093dafac88bd2c169cce
WangLiwei <948656028@qq.com>
2021-04-01 00:31:53 +0800

    modify git ingore fault

---
+++ cloudalibaba-provider-payment9004/src/main/java/com/atguigu/springcloud/alibaba/controller/PaymentController.java
@@ -0,0 +1,40 @@
+package com.atguigu.springcloud.alibaba.controller;
+
+import com.atguigu.springcloud.entities.CommonResult;
+import com.atguigu.springcloud.entities.Payment;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+
+/**
+ * @auther zzyy
+ * @create 2020-02-25 16:13
+ */
+@RestController
+public class PaymentController
+{
+    @Value("${server.port}")
+    private String serverPort;
+
+    public static HashMap<Integer,Payment> hashMap = new HashMap<>();
+    static
+    {
+        hashMap.put(1,new Payment(1,"28a8c1e3bc2742d8848569891fb42181"));
+        hashMap.put(2,new Payment(2,"bba8c1e3bc2742d8848569891ac32182"));
+        hashMap.put(3,new Payment(3,"6ua8c1e3bc2742d8848569891xt92183"));
+    }
+
+    @GetMapping(value = "/paymentSQL/{id}")
+    public CommonResult<Payment> paymentSQL(@PathVariable("id") Integer id)
+    {
+        Payment payment = hashMap.get(id);
+        CommonResult<Payment> result = new CommonResult(200,"from mysql,serverPort:  "+serverPort,payment);
+        return result;
+    }
+
+
+
+}
